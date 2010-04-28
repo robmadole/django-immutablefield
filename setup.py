8a7fb30d2903cb186f056f9ed7641c2859ecb9d2
Rob Madole <robmadole@gmail.com>
2010-04-28 15:07:39 -0500

    Fixing a setuptools problem where installing grabbed test dependencies

--- setup.py
+++ setup.py
@@ -5,12 +5,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-version = '0.1'
+version = '0.1.1'
 
 install_requires = []
-
-setup_requires = ['nose>=0.11.3', 'django>=1.1.1', 'py>=1.2.1', 'coverage>=3.3.1']
-
 
 setup(name='django-immutablefield',
     version=version,
@@ -33,6 +30,5 @@ setup(name='django-immutablefield',
     package_dir={'': 'src'},include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
-    setup_requires=setup_requires,
     entry_points={}
 )
